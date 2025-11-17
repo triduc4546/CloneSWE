d82bc7c3ff9c5381c9337a4abc5f87475c03dd69
Duckylalala <95141286+triduc4546@users.noreply.github.com>
2025-11-17 08:59:04 +0700

    Add files via upload

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import ProductsClient from "./ProductsClient";
 
 export default function Page() {
-  return <ProductsClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ProductsClient />
+    </Suspense>
+  );
 }
